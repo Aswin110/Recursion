fe4ad09608ecd7f7c7fd36a8fd68329ab3d782c8
Aswin110 <aswinashok110@gmail.com>
2023-10-08 23:30:00 +0530

    finish fibonacci

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 function fibs(n){
 	let f = 0, s = 1;
 	let arr = [];
+	if(n < 0){
+		console.log('enter a integer value greater than zero');
+        return;
+	}
 	for(let i = 0; i < n; i++){
 		arr.push(f);
 		let c = f + s;
@@ -10,14 +14,16 @@ function fibs(n){
 	console.log(arr);
 }
 
-fibs(10);
+fibs(-1);
 
 let f = 0, s = 1;
 let arr = [];
 
 function fibsRec(n){
-	if(n < 1){
+	if(n === 0){
 		console.log(arr);
+	}else if(n < 0){
+		console.log('enter a integer value greater than zero');
 	}else{
 		arr.push(f);
 		let c = f + s;
@@ -26,4 +32,4 @@ function fibsRec(n){
 		fibsRec(n-1);
 	}
 }
-fibsRec(0);
+fibsRec(-1);
